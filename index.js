a24095c42effe10d1cf78d23490d1eb9e3aa36b6
Mishal Shashikant <mishalmsb@gmail.com>
2016-05-10 01:51:48 +0100

    style bgimg btimg

--- index.js
+++ index.js
@@ -1,21 +1,17 @@
 $(init)
 function init() { 
 
-  $("#restartBt").hide();
- 
-var animePlayer = function() {
-    $('#plImg1').fadeOut("80");
-    $('#plImg2').fadeIn("90");
-    $('#plImg2').fadeOut("350");
-    $('#plImg3').fadeIn("300");
-    $('#plImg3').fadeOut("500");
-    $('#plImg1').fadeIn("90");
-}
+  // $("#btRestart").hide();
+  
+  $("#goalDiv").hide();
+  $("#missedDiv").hide();
+
+
 
   $("button").click(function() {
     
       // console.log(this.id; 
-    if (this.id=="btLeftBottom"){startKickOff(-30,210,1)}
+    if (this.id=="btLeftBottom"){startKickOff(-25,210,1)}
     else if (this.id=="btLeftCentre"){startKickOff(-55,213,2)}
     else if (this.id=="btLeftTop"){startKickOff(-90,213,3)}
     else if (this.id=="btCentreCentre"){startKickOff(-40,345,4)}
@@ -23,20 +19,33 @@ var animePlayer = function() {
     else if (this.id=="btRigthBottom"){startKickOff(-30,475,6)}
     else if (this.id=="btRigthCentre"){startKickOff(-55,475,7)}
     else if (this.id=="btRigthTop"){startKickOff(-90,475,8)}
-    else if (this.id=="btRestart"){MoveBall(150, 345); keeperMove(0,310,125)}
+    else if (this.id=="btRestart"){restartGame()}
 
   });
 
+  var restartGame = function() {
+
+
+      $('#plScored').text("");
+      $('#p1ResultDiv').text(0);
+      $('#p2ResultDiv').text(0);
+      $('#counterDiv').text(0);
+      $('#plMove').text("Player 1 Turn");
+      $("#btRestart").hide();
+      $("#allBt").show();
+ 
+  }
+
   var startKickOff = function(ballPosX, ballPosY, plMove) {
       
-      run([0,3,6,0]);
+      // run([0,3,6,0]);
 
-      $("#player").animateSprite('restart');
+      // $("#player").animateSprite('restart');
       
 
       var counter = parseInt($('#counterDiv').text()) + 1;
 
-      animePlayer();
+      // animePlayer();
       var comMove = computerMove(plMove);
       checkMoves(plMove,comMove);
       // console.log(plMove,comMove);
@@ -54,15 +63,23 @@ var animePlayer = function() {
           }
           
       },3000);
-      
-      if (counter < 4) { 
+      setTimeout(function() {
+       
+        $("#goalDiv").hide();
+        $("#missedDiv").hide();
+      },4000);
+
+      if (counter < 2) { 
         console.log(counter);
         $('#counterDiv').text(counter);
       } else {
         $('#counterDiv').text("GAME OVER");
-          $("#restartBt").hide();
-          $("#restartBt").show();
-      }
+          $("#allBt").hide();
+          $("#btRestart").show();
+          $('#plMove').text("");
+      };
+      $("#goalDiv").hide();
+
       
   };
 
@@ -80,27 +97,34 @@ var animePlayer = function() {
 
   var ballInside = function(inOut) {
 
+      
+
       // console.log("margin" + $(".ball").css("margin-left"))
       // var marLeft = parseInt("margin" + $(".ball").css("margin-left")) - 345
-
+          
       if(inOut=="in") {
           console.log("margin" + $(".ball").css("margin-left"));
-
-          setTimeout(function() {
-              console.log("margin" + $(".ball").css("margin-left"));
-              $(".ball").css({"margin-top": "-40px","margin-right": "0px","margin-bottom": "0px","margin-left": "250px"});
-          }, 900);
-
+       
+            setTimeout(function() {
+                console.log("margin" + $(".ball").css("margin-left"));
+                $(".ball").css({"margin-top": "-40px","margin-right": "0px","margin-bottom": "0px","margin-left": "350px"});
+            }, 900);
+            setTimeout(function() {
+               $("#goalDiv").show();
+            }, 2500);
+ 
           console.log("margin" + $(".ball").css("margin-left"));
           // setTimeout(function() {
           //     $(".ball").css({"margin-top": "-25px","margin-right": "0px","margin-bottom": "0px","margin-left": "260px"});
           // }, 1500);
-      } else if (inOut=="out") {
+      } else if (inOut=="out") { 
  
           setTimeout(function() {
-              $(".ball").css({"margin-top": "30px","margin-right": "0px","margin-bottom": "0px","margin-left": "170px"});
+              $(".ball").css({"margin-top": "30px","margin-right": "0px","margin-bottom": "0px","margin-left": "240px"});
           }, 900);
-          // console.log("margin" + $(".ball").css("margin-left"));
+          setTimeout(function() {
+               $("#missedDiv").show();
+          }, 2500);
       }
 
 
@@ -175,9 +199,7 @@ var animePlayer = function() {
       
   }
 
-  var playerMove = function() {
-
-  }
+  
 
   var checkMoves = function(playerMove, computerMove) {
       if (playerMove==computerMove) {
@@ -225,19 +247,19 @@ var animePlayer = function() {
       // console.log(p1, p2);
   }
 
-  var run = function(walkRight) {
-      $(function() {
-          $("#player").animateSprite({
-              fps: 15,
-              animations: {
-                  walk: walkRight
-                  // walkLeft: [15, 14, 13, 12, 11, 10, 9, 8],
-                  // walkCrazy: [0, 14, 2, 12, 3, 10, 4, 8]
-              },
-              loop: false,
-          });
-      });      
-  }
+  // var run = function(walkRight) {
+  //     $(function() {
+  //         $("#player").animateSprite({
+  //             fps: 15,
+  //             animations: {
+  //                 walk: walkRight
+  //                 // walkLeft: [15, 14, 13, 12, 11, 10, 9, 8],
+  //                 // walkCrazy: [0, 14, 2, 12, 3, 10, 4, 8]
+  //             },
+  //             loop: false,
+  //         });
+  //     });      
+  // }
   
 
 
@@ -247,7 +269,14 @@ var animePlayer = function() {
   //     console.log("Y" + (e.pageY - offset.top));
   //   })
   
-  
+  // var animePlayer = function() {
+  //     $('#plImg1').fadeOut("80");
+  //     $('#plImg2').fadeIn("90");
+  //     $('#plImg2').fadeOut("350");
+  //     $('#plImg3').fadeIn("300");
+  //     $('#plImg3').fadeOut("500");
+  //     $('#plImg1').fadeIn("90");
+  // }
 
 }
 
